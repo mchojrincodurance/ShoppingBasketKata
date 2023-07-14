3b01e02c563ebe06f2b373e7acac8783b6589645
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-14 18:25:00 +0200

    Inner TDD loop: added new dependency on product repository

--- src/main/java/com/codurance/shoppingbasket/controller/MainController.java
+++ src/main/java/com/codurance/shoppingbasket/controller/MainController.java
@@ -1,19 +1,27 @@
 package com.codurance.shoppingbasket.controller;
 
 import com.codurance.shoppingbasket.infrastructure.MyConsole;
+import com.codurance.shoppingbasket.repositories.ProductRepository;
 import com.codurance.shoppingbasket.service.ShoppingBasketService;
 
 public class MainController {
     private final ShoppingBasketService shoppingBasketService;
+    private ProductRepository productRepository;
 
-    public MainController(MyConsole console, ShoppingBasketService shoppingBasketService) {
+    public MainController(MyConsole console, ShoppingBasketService shoppingBasketService, ProductRepository productRepository) {
         this.shoppingBasketService = shoppingBasketService;
+        this.productRepository = productRepository;
     }
 
     public void checkBasketContent(int i) {
     }
 
     public void addItem(int userId, String productName, int quantity) {
-        shoppingBasketService.addItem(1, 1, 2);
+        shoppingBasketService.addItem(userId, findIdFor(productName), quantity);
+    }
+
+    private int findIdFor(String productName) {
+
+        return productRepository.find(productName).id();
     }
 }
