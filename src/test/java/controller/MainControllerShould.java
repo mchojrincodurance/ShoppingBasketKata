02b963b20e767f819730360027c67c6ca167d136
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-14 18:03:42 +0200

    Inner TDD loop: refactor, moving to a parameterized test implementation

--- src/test/java/controller/MainControllerShould.java
+++ src/test/java/controller/MainControllerShould.java
@@ -2,17 +2,24 @@ package controller;
 
 import com.codurance.shoppingbasket.controller.MainController;
 import com.codurance.shoppingbasket.infrastructure.MyConsole;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import com.codurance.shoppingbasket.service.ShoppingBasketService;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.stream.Stream;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class MainControllerShould {
 
     public static final int USER_ID = 1;
@@ -23,8 +30,15 @@ public class MainControllerShould {
     @Mock
     private ShoppingBasketService shoppingBasketService;
 
-    @Test
-    public void allow_adding_items() {
+    public static Stream<Arguments> provideAddingItemsParameters() {
+        return Stream.of(
+                Arguments.of(USER_ID, PRODUCT_NAME, THE_HOBBIT_QUANTITY)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideAddingItemsParameters")
+    public void allow_adding_items(int userId, String productName, int quantity) {
         MainController mainController = new MainController(console, shoppingBasketService);
 
         mainController.addItem(USER_ID, PRODUCT_NAME, THE_HOBBIT_QUANTITY);
