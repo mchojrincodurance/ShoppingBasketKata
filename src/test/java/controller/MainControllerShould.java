ef7b206e4121bd7209e80706da1a0684e06fafd9
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-14 18:08:04 +0200

    Inner TDD loop: failing test for a second product

--- src/test/java/controller/MainControllerShould.java
+++ src/test/java/controller/MainControllerShould.java
@@ -3,15 +3,11 @@ package controller;
 import com.codurance.shoppingbasket.controller.MainController;
 import com.codurance.shoppingbasket.infrastructure.MyConsole;
 import com.codurance.shoppingbasket.service.ShoppingBasketService;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.runners.Parameterized;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.stream.Stream;
@@ -23,26 +19,31 @@ import static org.mockito.Mockito.verify;
 public class MainControllerShould {
 
     public static final int USER_ID = 1;
-    public static final String PRODUCT_NAME = "The hobbit";
-    public static final int THE_HOBBIT_QUANTITY = 2;
-    public static final int THE_HOBBIT_ID = 1;
+    public static final String FIRST_PRODUCT = "The hobbit";
+    public static final int FIRST_PRODUCT_QUANTITY = 2;
+    public static final int FIRST_PRODUCT_ID = 1;
+    public static final String SECOND_PRODUCT = "Breaking Bad";
+    public static final int SECOND_PRODUCT_QUANTITY = 5;
+    public static final int SECOND_PRODUCT_ID = 2;
+
     private final MyConsole console = new MyConsole();
     @Mock
     private ShoppingBasketService shoppingBasketService;
 
     public static Stream<Arguments> provideAddingItemsParameters() {
         return Stream.of(
-                Arguments.of(USER_ID, PRODUCT_NAME, THE_HOBBIT_QUANTITY)
+                Arguments.of(USER_ID, FIRST_PRODUCT, FIRST_PRODUCT_ID, FIRST_PRODUCT_QUANTITY),
+                Arguments.of(USER_ID, SECOND_PRODUCT, SECOND_PRODUCT_ID, SECOND_PRODUCT_QUANTITY)
         );
     }
 
     @ParameterizedTest
     @MethodSource("provideAddingItemsParameters")
-    public void allow_adding_items(int userId, String productName, int quantity) {
+    public void allow_adding_items(int userId, String productName, int productId, int quantity) {
         MainController mainController = new MainController(console, shoppingBasketService);
 
-        mainController.addItem(USER_ID, PRODUCT_NAME, THE_HOBBIT_QUANTITY);
+        mainController.addItem(userId, productName, quantity);
 
-        verify(shoppingBasketService).addItem(eq(USER_ID), eq(THE_HOBBIT_ID), eq(THE_HOBBIT_QUANTITY));
+        verify(shoppingBasketService).addItem(eq(userId), eq(productId), eq(quantity));
     }
 }
