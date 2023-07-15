2b52a91951919d2db3193dac5db737cfed09e0dc
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-15 15:48:48 +0200

    Completed implementation of ShoppingBasketFactory

--- src/main/java/com/codurance/shoppingbasket/model/ShoppingBasketFactory.java
+++ src/main/java/com/codurance/shoppingbasket/model/ShoppingBasketFactory.java
@@ -3,6 +3,6 @@ package com.codurance.shoppingbasket.model;
 public class ShoppingBasketFactory {
     public ShoppingBasket create(int userId) {
 
-        return null;
+        return new ShoppingBasket(userId);
     }
 }
---
+++ src/test/java/model/ShoppingBasketFactoryShould.java
@@ -0,0 +1,24 @@
+package model;
+
+import com.codurance.shoppingbasket.model.ShoppingBasket;
+import com.codurance.shoppingbasket.model.ShoppingBasketFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ShoppingBasketFactoryShould {
+    private ShoppingBasketFactory shoppingBasketFactory = new ShoppingBasketFactory();
+
+    @ParameterizedTest
+    @CsvSource({
+            "1",
+            "2",
+            "3"
+    })
+    public void create_baskets_for_users(int userId) {
+        ShoppingBasket createdBasket = shoppingBasketFactory.create(userId);
+        assertEquals(userId, createdBasket.ownerId());
+    }
+}
