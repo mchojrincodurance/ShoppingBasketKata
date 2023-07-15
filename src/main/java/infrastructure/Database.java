ffb8fc3e8c526718bb88b7579516e6c3fb20f1d3
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-15 14:57:03 +0200

    Fixed implementation of stub database

--- src/main/java/infrastructure/Database.java
+++ src/main/java/infrastructure/Database.java
@@ -9,8 +9,16 @@ import java.util.HashMap;
 public class Database {
 
     private final HashMap<String, ArrayList<Object>> records = new HashMap<String, ArrayList<Object>>() {{
-        put("products", new ArrayList<Object>(Arrays.asList(new Product(10001, "Lord of the Rings", 10))));
-        put("shopping_baskets", new ArrayList<Object>());
+        put("product", new ArrayList<Object>(
+                        Arrays.asList(
+                                new Product(10001, "Lord of the Rings", 10),
+                                new Product(10002, "The hobbit", 5),
+                                new Product(10002, "Game of Thrones", 9),
+                                new Product(10002, "Breaking Bad", 7)
+                        )
+                )
+        );
+        put("shopping_basket", new ArrayList<Object>());
     }};
 
     public Object findBy(String ObjectType, String field, String value) {
@@ -19,8 +27,10 @@ public class Database {
             return records
                     .get(ObjectType)
                     .stream()
-                    .filter((element) -> ((Product)element).name().equals(value))
-                    .findFirst();
+                    .filter((element) -> ((Product) element).name().equals(value))
+                    .findFirst()
+                    .get()
+                    ;
         }
 
         throw new UnsupportedOperationException("infrastructure.Database::findBy not implemented yet");
