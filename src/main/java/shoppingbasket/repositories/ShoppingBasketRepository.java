2171966ea29486d0bf4455e1b2ec74831cd2f575
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-15 16:00:27 +0200

    Adding a skeleton for the ShoppingBasketRepository

---
+++ src/main/java/shoppingbasket/repositories/ShoppingBasketRepository.java
@@ -0,0 +1,9 @@
+package shoppingbasket.repositories;
+
+import com.codurance.shoppingbasket.model.ShoppingBasket;
+
+public class ShoppingBasketRepository {
+    public void save(ShoppingBasket shoppingBasket) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
