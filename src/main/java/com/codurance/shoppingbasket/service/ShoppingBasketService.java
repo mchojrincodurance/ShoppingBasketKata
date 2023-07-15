6e5a9849bcbbbeb8db4ec55768a84fc6e462e6be
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-15 15:39:05 +0200

    Adding the dependency on ShoppingBasketFactory to check that the creation is made in the expected conditions

--- src/main/java/com/codurance/shoppingbasket/service/ShoppingBasketService.java
+++ src/main/java/com/codurance/shoppingbasket/service/ShoppingBasketService.java
@@ -1,16 +1,25 @@
 package com.codurance.shoppingbasket.service;
 
 import com.codurance.shoppingbasket.model.ShoppingBasket;
+import com.codurance.shoppingbasket.model.ShoppingBasketFactory;
 
 import java.util.HashMap;
 
 public class ShoppingBasketService {
     private final HashMap<String, ShoppingBasket> shoppingBaskets = new HashMap<>();
+    private final ShoppingBasketFactory shoppingBasketFactory;
+
+    public ShoppingBasketService(ShoppingBasketFactory shoppingBasketFactory)
+    {
+        this.shoppingBasketFactory = shoppingBasketFactory;
+    }
 
     public void addItem(int userId, int itemId, int quantity) {
         if (!shoppingBaskets.containsKey(Integer.toString(userId))) {
-            shoppingBaskets.put(Integer.toString(userId), new ShoppingBasket(userId));
+            shoppingBaskets.put(Integer.toString(userId), shoppingBasketFactory.create(userId));
         }
+
+//        basketFor(userId).add(new Product(itemId, "", 10));
     }
 
     public ShoppingBasket basketFor(int userId) {
