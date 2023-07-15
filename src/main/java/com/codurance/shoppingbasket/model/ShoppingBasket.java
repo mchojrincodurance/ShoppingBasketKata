3101db25a4efd7ecf9af17c2143ef6cbda374c87
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-15 15:19:30 +0200

    Initial implementation of the ShoppingBasket class

--- src/main/java/com/codurance/shoppingbasket/model/ShoppingBasket.java
+++ src/main/java/com/codurance/shoppingbasket/model/ShoppingBasket.java
@@ -1,4 +1,20 @@
 package com.codurance.shoppingbasket.model;
 
+import java.util.ArrayList;
+
 public class ShoppingBasket {
+    private final int ownerId;
+
+    public ShoppingBasket(int ownerId)
+    {
+        this.ownerId = ownerId;
+    }
+
+    public int ownerId() {
+        return ownerId;
+    }
+
+    public ArrayList products() {
+        throw new UnsupportedOperationException("com.codurance.shoppingbasket.model.ShoppingBasket::ownerId not implemented yet");
+    }
 }
