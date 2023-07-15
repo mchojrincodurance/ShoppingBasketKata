304ee1ef06cf8c9f9b8e419179c228cb80415065
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-15 16:16:41 +0200

    Validating that the shoppingBasketService adds products to the basket

--- src/test/java/shoppingbasket/service/ShoppingBasketServiceShould.java
+++ src/test/java/shoppingbasket/service/ShoppingBasketServiceShould.java
@@ -1,5 +1,6 @@
 package shoppingbasket.service;
 
+import com.codurance.shoppingbasket.model.Product;
 import com.codurance.shoppingbasket.model.ShoppingBasket;
 import com.codurance.shoppingbasket.model.ShoppingBasketFactory;
 import com.codurance.shoppingbasket.service.ShoppingBasketService;
@@ -9,10 +10,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import shoppingbasket.model.ProductOrder;
 import shoppingbasket.repositories.ShoppingBasketRepository;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -20,6 +21,9 @@ import static org.mockito.Mockito.verify;
 public class ShoppingBasketServiceShould {
     public static final int USER_ID = 1;
     public static final int ITEM_ID = 1;
+    private static final String ITEM_NAME = "An item";
+    private static final int ITEM_QUANTITY = 2;
+    private static final int ITEM_PRICE = 6;
     @Spy
     private ShoppingBasketFactory shoppingBasketFactory;
     @InjectMocks
@@ -55,5 +59,15 @@ public class ShoppingBasketServiceShould {
         ShoppingBasket shoppingBasket = shoppingBasketService.basketFor(USER_ID);
 
         verify(shoppingBasketRepository).save(shoppingBasket);
+    }
+
+    @Test
+    public void add_items_to_the_shopping_basket() {
+        shoppingBasketService.addItem(USER_ID, ITEM_ID, ITEM_QUANTITY);
+        ShoppingBasket shoppingBasket = shoppingBasketService.basketFor(USER_ID);
+
+        ProductOrder productOrder = new ProductOrder(new Product(ITEM_ID, ITEM_NAME, ITEM_PRICE), ITEM_QUANTITY);
+
+        assertTrue(shoppingBasket.productOrders().contains(productOrder));
     }
 }
