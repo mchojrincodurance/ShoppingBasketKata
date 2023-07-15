2c6f421861b2afc7aa647f9120e160dab3fa6795
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-15 15:54:27 +0200

    Switching to a spy to use the real implementation of the create method in the ShoppingBasket factory

--- src/test/java/shoppingbasket/service/ShoppingBasketServiceShould.java
+++ src/test/java/shoppingbasket/service/ShoppingBasketServiceShould.java
@@ -6,9 +6,10 @@ import com.codurance.shoppingbasket.service.ShoppingBasketService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -18,7 +19,7 @@ public class ShoppingBasketServiceShould {
     public static final int USER_ID = 1;
     public static final int ITEM_ID = 1;
     public static final String ITEM_NAME = "A product";
-    @Mock
+    @Spy
     private ShoppingBasketFactory shoppingBasketFactory;
     @InjectMocks
     private ShoppingBasketService shoppingBasketService;
@@ -30,5 +31,18 @@ public class ShoppingBasketServiceShould {
 
         shoppingBasketService.addItem(USER_ID, ITEM_ID, 1);
         verify(shoppingBasketFactory, times(1)).create(USER_ID);
+    }
+
+    @Test
+    public void create_different_shopping_baskets_for_different_users() {
+        shoppingBasketService = new ShoppingBasketService(shoppingBasketFactory);
+
+        shoppingBasketService.addItem(1, 1, 2);
+        shoppingBasketService.addItem(2, 1, 2);
+
+        ShoppingBasket firstUserBasket = shoppingBasketService.basketFor(1);
+        ShoppingBasket secondUserBasket = shoppingBasketService.basketFor(2);
+
+        assertNotEquals(firstUserBasket, secondUserBasket);
     }
 }
