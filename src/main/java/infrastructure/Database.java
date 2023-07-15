50540cec69d52b0f20dbe14aae6b47448d55191f
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-15 14:48:47 +0200

    Initial implementation of stub database

--- src/main/java/infrastructure/Database.java
+++ src/main/java/infrastructure/Database.java
@@ -2,27 +2,25 @@ package infrastructure;
 
 import com.codurance.shoppingbasket.model.Product;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Database {
-    private final Product[] products = {
-            new Product(10001, "Lord of the Rings", 10),
-            new Product(10002, "The Hobbit", 5),
-            new Product(20001, "Game of Thrones", 9),
-            new Product(20110, "Breaking Bad", 7),
-    };
+
+    private final HashMap<String, ArrayList<Object>> records = new HashMap<String, ArrayList<Object>>() {{
+        put("products", new ArrayList<Object>(Arrays.asList(new Product(10001, "Lord of the Rings", 10))));
+        put("shopping_baskets", new ArrayList<Object>());
+    }};
 
     public Object findBy(String ObjectType, String field, String value) {
-        if (ObjectType.equals("product")) {
+        if (field.equals("name")) {
 
-            if (field.equals("name")) {
-
-                return Arrays
-                        .stream(products)
-                        .filter((Product element) -> element.name().equals(value))
-                        .findFirst()
-                        ;
-            }
+            return records
+                    .get(ObjectType)
+                    .stream()
+                    .filter((element) -> ((Product)element).name().equals(value))
+                    .findFirst();
         }
 
         throw new UnsupportedOperationException("infrastructure.Database::findBy not implemented yet");
