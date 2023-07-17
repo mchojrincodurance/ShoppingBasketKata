4ead595f3cb957429926b335b1a37d2ad4d1735d
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-17 13:32:40 +0200

    Moving the initialization of the db to the acceptance test

--- src/main/java/com/codurance/shoppingbasket/infrastructure/Database.java
+++ src/main/java/com/codurance/shoppingbasket/infrastructure/Database.java
@@ -16,44 +16,10 @@ public class Database {
     public static final String ID = "id";
     public static final String NAME = "name";
     public static final String PRICE = "price";
-    public static final String LORD_OF_THE_RINGS_ID = "10001";
-    public static final String LORD_OF_THE_RINGS_NAME = "Lord of the Rings";
-    public static final String LORD_OF_THE_RINGS_PRICE = "10";
-    public static final String THE_HOBBIT_ID = "10002";
-    public static final String THE_HOBBIT_NAME = "The Hobbit";
-    public static final String THE_HOBBIT_PRICE = "5";
-    public static final String GAME_OF_THRONES_ID = "20001";
-    public static final String GAME_OF_THRONES_NAME = "Game of Thrones";
-    public static final String GAME_OF_THRONES_PRICE = "9";
-    public static final String BREAKING_BAD_ID = "20110";
-    public static final String BREAKING_BAD_NAME = "Breaking Bad";
-    public static final String BREAKING_BAD_PRICE = "7";
     private int nextObjectId = 1;
 
     private final HashMap<String, HashMap<String, HashMap<String, String>>> records = new HashMap<>() {{
-        put(PRODUCT, new HashMap<>() {{
-            put(LORD_OF_THE_RINGS_ID, new HashMap<>() {{
-                put(ID, LORD_OF_THE_RINGS_ID);
-                put(NAME, LORD_OF_THE_RINGS_NAME);
-                put(PRICE, LORD_OF_THE_RINGS_PRICE);
-            }});
-            put(THE_HOBBIT_ID, new HashMap<>() {{
-                put(ID, THE_HOBBIT_ID);
-                put(NAME, THE_HOBBIT_NAME);
-                put(PRICE, THE_HOBBIT_PRICE);
-            }});
-            put(GAME_OF_THRONES_ID, new HashMap<>() {{
-                put(ID, GAME_OF_THRONES_ID);
-                put(NAME, GAME_OF_THRONES_NAME);
-                put(PRICE, GAME_OF_THRONES_PRICE);
-            }});
-            put(BREAKING_BAD_ID, new HashMap<>() {{
-                put(ID, BREAKING_BAD_ID);
-                put(NAME, BREAKING_BAD_NAME);
-                put(PRICE, BREAKING_BAD_PRICE);
-            }});
-        }});
-
+        put(PRODUCT, new HashMap<>());
         put(SHOPPING_BASKET, new HashMap<>());
         put(PRODUCT_ORDER, new HashMap<>());
     }};
@@ -72,10 +38,9 @@ public class Database {
     public int insert(String objectType, HashMap<String, String> assignments) {
         String id = assignments.containsKey(ID) ? assignments.get(ID) : Integer.toString(nextObjectId++);
 
-        records.get(objectType)
-                .put(
-                        id, assignments
-                );
+        records
+                .get(objectType)
+                .put(id, assignments);
 
         return Integer.parseInt(id);
     }
--- src/test/java/com.codurance.shoppingbasket/feature/addItemsFeature.java
+++ src/test/java/com.codurance.shoppingbasket/feature/addItemsFeature.java
@@ -17,6 +17,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import com.codurance.shoppingbasket.repositories.ShoppingBasketRepository;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -28,6 +29,18 @@ public class addItemsFeature {
     public static final int THE_HOBBIT_QUANTITY = 2;
     public static final int BREAKING_BAD_QUANTITY = 5;
     public static final int USER_ID = 1;
+    public static final String LORD_OF_THE_RINGS_ID = "10001";
+    public static final String LORD_OF_THE_RINGS_NAME = "Lord of the Rings";
+    public static final String LORD_OF_THE_RINGS_PRICE = "10";
+    public static final String THE_HOBBIT_ID = "10002";
+    public static final String THE_HOBBIT_NAME = "The Hobbit";
+    public static final String THE_HOBBIT_PRICE = "5";
+    public static final String GAME_OF_THRONES_ID = "20001";
+    public static final String GAME_OF_THRONES_NAME = "Game of Thrones";
+    public static final String GAME_OF_THRONES_PRICE = "9";
+    public static final String BREAKING_BAD_ID = "20110";
+    public static final String BREAKING_BAD_NAME = "Breaking Bad";
+    public static final String BREAKING_BAD_PRICE = "7";
     private final Database database = new Database();
     private final ProductRepository productRepository = new ProductRepository(database);
     private final ShoppingBasketRepository shoppingBasketRepository = new ShoppingBasketRepository(database);
@@ -48,6 +61,7 @@ public class addItemsFeature {
                 shoppingBasketRepository,
                 productRepository
         ), productRepository);
+        initDatabase();
     }
 
     /**
@@ -74,5 +88,28 @@ public class addItemsFeature {
         verify(console).printLine("2 x The Hobbit // 2 x 5.00 = £10.00");
         verify(console).printLine("5 x Breaking Bad // 5 x 7.00 = £35.00");
         verify(console).printLine("Total: £45.00");
+    }
+
+    private void initDatabase() {
+        database.insert(Database.PRODUCT, new HashMap<>() {{
+            put(Database.ID, LORD_OF_THE_RINGS_ID);
+            put(Database.NAME, LORD_OF_THE_RINGS_NAME);
+            put(Database.PRICE, LORD_OF_THE_RINGS_PRICE);
+        }});
+        database.insert(Database.PRODUCT, new HashMap<>() {{
+            put(Database.ID, THE_HOBBIT_ID);
+            put(Database.NAME, THE_HOBBIT_NAME);
+            put(Database.PRICE, THE_HOBBIT_PRICE);
+        }});
+        database.insert(Database.PRODUCT, new HashMap<>() {{
+            put(Database.ID, GAME_OF_THRONES_ID);
+            put(Database.NAME, GAME_OF_THRONES_NAME);
+            put(Database.PRICE, GAME_OF_THRONES_PRICE);
+        }});
+        database.insert(Database.PRODUCT, new HashMap<>() {{
+            put(Database.ID, BREAKING_BAD_ID);
+            put(Database.NAME, BREAKING_BAD_NAME);
+            put(Database.PRICE, BREAKING_BAD_PRICE);
+        }});
     }
 }
