49e395d4b8b156e10f1c02ce4731183e9dd81cb4
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-17 16:24:01 +0200

    Refactor: fixed test

--- src/test/java/com.codurance.shoppingbasket/repositories/ProductRepositoryShould.java
+++ src/test/java/com.codurance.shoppingbasket/repositories/ProductRepositoryShould.java
@@ -16,7 +16,6 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class ProductRepositoryShould {
     public static final String PRODUCT_NAME = "The hobbit";
-    public static final String PRODUCT_TYPE = "product";
     public static final float PRODUCT_PRICE = 5F;
     public static final int PRODUCT_ID = 1;
     @Mock
@@ -28,9 +27,10 @@ public class ProductRepositoryShould {
     public void find_products_by_name() {
         Product theHobbit = new Product(PRODUCT_ID, PRODUCT_NAME, PRODUCT_PRICE);
 
-        when(database.findBy(PRODUCT_TYPE, Database.NAME, PRODUCT_NAME)).thenReturn(buildProductRecord());
+        when(database.findBy(Database.PRODUCT, Database.NAME, PRODUCT_NAME)).thenReturn(buildProductRecord());
 
         Product actual = productRepository.find(PRODUCT_NAME);
+
         assertEquals(theHobbit.id(), actual.id());
         assertEquals(theHobbit.name(), actual.name());
         assertEquals(theHobbit.price(), actual.price());
@@ -40,9 +40,10 @@ public class ProductRepositoryShould {
     public void find_products_by_id() {
         Product theHobbit = new Product(PRODUCT_ID, PRODUCT_NAME, PRODUCT_PRICE);
 
-        when(database.findBy(PRODUCT_TYPE, Database.ID, Integer.toString(PRODUCT_ID))).thenReturn(buildProductRecord());
+        when(database.findBy(Database.PRODUCT, Database.ID, Integer.toString(PRODUCT_ID))).thenReturn(buildProductRecord());
 
         Product actual = productRepository.find(PRODUCT_ID);
+
         assertEquals(theHobbit.id(), actual.id());
         assertEquals(theHobbit.name(), actual.name());
         assertEquals(theHobbit.price(), actual.price());
@@ -50,6 +51,7 @@ public class ProductRepositoryShould {
 
     private static HashMap<String, String> buildProductRecord() {
         return new HashMap<>() {{
+            put(Database.ID, Integer.toString(PRODUCT_ID));
             put(Database.NAME, PRODUCT_NAME);
             put(Database.PRICE, Float.toString(PRODUCT_PRICE));
         }};
