64ac6e51f37bd965680d6ebc9dc25f706309497e
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-17 09:00:27 +0200

    Adding the responsibility of keeping track of shopping basket creation date to the ShoppingBasketFactory

--- src/main/java/com/codurance/shoppingbasket/model/ShoppingBasketFactory.java
+++ src/main/java/com/codurance/shoppingbasket/model/ShoppingBasketFactory.java
@@ -1,8 +1,17 @@
 package com.codurance.shoppingbasket.model;
 
+import com.codurance.shoppingbasket.infrastructure.MyClock;
+
 public class ShoppingBasketFactory {
+    private MyClock clock;
+
+    public ShoppingBasketFactory(MyClock clock)
+    {
+        this.clock = clock;
+    }
+
     public ShoppingBasket create(int userId) {
 
-        return new ShoppingBasket(userId);
+        return new ShoppingBasket(userId, clock.getCurrentDate());
     }
 }
--- src/test/java/com.codurance.shoppingbasket/model/ShoppingBasketFactoryShould.java
+++ src/test/java/com.codurance.shoppingbasket/model/ShoppingBasketFactoryShould.java
@@ -1,15 +1,25 @@
 package com.codurance.shoppingbasket.model;
 
-import com.codurance.shoppingbasket.model.ShoppingBasket;
-import com.codurance.shoppingbasket.model.ShoppingBasketFactory;
-import org.junit.jupiter.api.Test;
+import com.codurance.shoppingbasket.infrastructure.MyClock;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class ShoppingBasketFactoryShould {
-    private ShoppingBasketFactory shoppingBasketFactory = new ShoppingBasketFactory();
+    @Mock
+    MyClock clock;
+
+    @InjectMocks
+    private ShoppingBasketFactory shoppingBasketFactory;
 
     @ParameterizedTest
     @CsvSource({
@@ -20,5 +30,20 @@ public class ShoppingBasketFactoryShould {
     public void create_baskets_for_users(int userId) {
         ShoppingBasket createdBasket = shoppingBasketFactory.create(userId);
         assertEquals(userId, createdBasket.ownerId());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2022-12-22",
+            "2023-02-01"
+    })
+    public void keep_track_of_baskets_creation_date(String dateString) {
+        LocalDate date = LocalDate.parse(dateString);
+
+        when(clock.getCurrentDate()).thenReturn(date);
+
+        ShoppingBasket createdBasket = shoppingBasketFactory.create(1);
+
+        assertEquals(date, createdBasket.createdAt());
     }
 }
