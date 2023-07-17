6830d117ff3cf1cbb118e2f7acfc110a24c3e9fb
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-17 16:52:46 +0200

    Refactor: changed test implementation to not require a mock for the ShoppingBasketFactory

--- src/test/java/com.codurance.shoppingbasket/service/ShoppingBasketServiceShould.java
+++ src/test/java/com.codurance.shoppingbasket/service/ShoppingBasketServiceShould.java
@@ -1,19 +1,20 @@
 package com.codurance.shoppingbasket.service;
 
+import com.codurance.shoppingbasket.infrastructure.MyClock;
 import com.codurance.shoppingbasket.model.Product;
 import com.codurance.shoppingbasket.model.ShoppingBasket;
 import com.codurance.shoppingbasket.model.ShoppingBasketFactory;
 import com.codurance.shoppingbasket.repositories.ProductRepository;
+import com.codurance.shoppingbasket.repositories.ShoppingBasketRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import com.codurance.shoppingbasket.repositories.ShoppingBasketRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ShoppingBasketServiceShould {
@@ -23,9 +24,6 @@ public class ShoppingBasketServiceShould {
     private static final int ITEM_QUANTITY = 2;
     private static final int ITEM_PRICE = 6;
     private static final int OTHER_USER_ID = 2;
-    @Spy
-    private ShoppingBasketFactory shoppingBasketFactory;
-    @InjectMocks
     private ShoppingBasketService shoppingBasketService;
     @Mock
     private ShoppingBasketRepository shoppingBasketRepository;
@@ -33,19 +31,25 @@ public class ShoppingBasketServiceShould {
     @Mock
     private ProductRepository productRepository;
 
+    @BeforeEach
+    public void setUp()
+    {
+        shoppingBasketService = new ShoppingBasketService(new ShoppingBasketFactory(new MyClock()), shoppingBasketRepository, productRepository);
+    }
+
     @Test
     public void create_shopping_basket_when_first_product_is_added() {
         ShoppingBasket shoppingBasket = shoppingBasketService.basketFor(USER_ID);
         assertNull(shoppingBasket);
 
         shoppingBasketService.addItem(USER_ID, ITEM_ID, 1);
-        verify(shoppingBasketFactory, times(1)).create(USER_ID);
+
+        shoppingBasket = shoppingBasketService.basketFor(USER_ID);
+        assertNotNull(shoppingBasket);
     }
 
     @Test
     public void create_different_shopping_baskets_for_different_users() {
-        shoppingBasketService = new ShoppingBasketService(shoppingBasketFactory, shoppingBasketRepository, productRepository);
-
         shoppingBasketService.addItem(USER_ID, ITEM_ID, 2);
         shoppingBasketService.addItem(OTHER_USER_ID, ITEM_ID, 3);
 
