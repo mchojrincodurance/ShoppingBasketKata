30ebed76e96a6c762f615a754fa044a58602e222
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-07-17 09:17:53 +0200

    Verifying correctness of Shopping basket rendering

--- src/main/java/com/codurance/shoppingbasket/infrastructure/ShoppingBasketRenderer.java
+++ src/main/java/com/codurance/shoppingbasket/infrastructure/ShoppingBasketRenderer.java
@@ -1,13 +1,37 @@
 package com.codurance.shoppingbasket.infrastructure;
 
 import com.codurance.shoppingbasket.model.ShoppingBasket;
+import shoppingbasket.model.ProductOrder;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 public class ShoppingBasketRenderer {
+    private final MyConsole console;
+
     public ShoppingBasketRenderer(MyConsole console) {
-        throw new UnsupportedOperationException();
+        this.console = console;
     }
 
     public void render(ShoppingBasket shoppingBasket) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        console.printLine("Creation date of the shopping basket: " + shoppingBasket.createdAt().toString());
+
+        for (ProductOrder po : shoppingBasket.productOrders() ) {
+            render(po);
+        }
+
+        console.printLine("Total: £" + formatFloat(shoppingBasket.total()));
+    }
+
+    private void render(ProductOrder po) {
+        console.printLine(po.quantity() + " x " + po.product().name() + " // " + po.quantity() + " x " + formatFloat(po.product().price()) + " = £" + formatFloat(po.subtotal()));
+    }
+
+    private String formatFloat(float value)
+    {
+        DecimalFormatSymbols decimalSymbols = DecimalFormatSymbols.getInstance();
+        decimalSymbols.setDecimalSeparator('.');
+
+        return new DecimalFormat("0.00", decimalSymbols).format(value);
     }
 }
--- src/test/java/com.codurance.shoppingbasket/infrastructure/ShoppingBasketRendererShould.java
+++ src/test/java/com.codurance.shoppingbasket/infrastructure/ShoppingBasketRendererShould.java
@@ -1,13 +1,18 @@
 package com.codurance.shoppingbasket.infrastructure;
 
+import com.codurance.shoppingbasket.model.Product;
 import com.codurance.shoppingbasket.model.ShoppingBasket;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import shoppingbasket.model.ProductOrder;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.stream.Stream;
 
 import static org.mockito.Mockito.verify;
 
@@ -15,23 +20,67 @@ import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
 public class ShoppingBasketRendererShould {
 
-    private static final int ONWER_ID = 1;
-    @Mock
+    private static final int OWNER_ID = 1;
+    public static final String THE_HOBBIT = "The Hobbit";
+    public static final int THE_HOBBIT_ID = 1;
+    public static final int THE_HOBBIT_PRICE = 5;
+    public static final String PURCHASE_DATE = "2023-07-16";
+    public static final int THE_HOBBIT_ORDERED_QUANTITY = 2;
+    public static final int BREAKING_BAD_ID = 2;
+    public static final String BREAKING_BAD = "Breaking Bad";
+    public static final int BREAKING_BAD_PRICE = 7;
+    public static final int BREAKING_BAD_ORDERED_QUANTITY = 5;
+    public static final int OTHER_OWNER_ID = 2;
+    public static final String OTHER_PURCHASE_DATE = "2015-11-03";
+    public static final int ALICE_IN_WONDERLAND_ID = 3;
+    public static final String ALICE_IN_WONDERLAND = "Alice in Wonderland";
+    public static final float ALICE_IN_WONDERLAND_PRICE = 12.8F;
+    public static final int ALICE_IN_WONDERLAND_ORDERED_QUANTITY = 1;
+    @Spy
     private MyConsole console;
+
     @InjectMocks
     private ShoppingBasketRenderer renderer;
 
-    @Test
-    public void render_shopping_basket_to_the_console () {
-        Date today = new Date();
-        ShoppingBasket shoppingBasket = new ShoppingBasket(ONWER_ID, today);
+    public static Stream<Arguments> shoppingBasketProvider() {
+        Product theHobbit = new Product(THE_HOBBIT_ID, THE_HOBBIT, THE_HOBBIT_PRICE);
 
+        ShoppingBasket firstShoppingBasket = new ShoppingBasket(OWNER_ID, LocalDate.parse(PURCHASE_DATE));
+        firstShoppingBasket.add(new ProductOrder(theHobbit, THE_HOBBIT_ORDERED_QUANTITY));
+
+        Product breakingBad = new Product(BREAKING_BAD_ID, BREAKING_BAD, BREAKING_BAD_PRICE);
+        firstShoppingBasket.add(new ProductOrder(breakingBad, BREAKING_BAD_ORDERED_QUANTITY));
+
+        Product aliceInWonderland = new Product(ALICE_IN_WONDERLAND_ID, ALICE_IN_WONDERLAND, ALICE_IN_WONDERLAND_PRICE);
+
+        ShoppingBasket secondShoppingBasket = new ShoppingBasket(OTHER_OWNER_ID, LocalDate.parse(OTHER_PURCHASE_DATE));
+        secondShoppingBasket.add(new ProductOrder(aliceInWonderland, ALICE_IN_WONDERLAND_ORDERED_QUANTITY));
+        secondShoppingBasket.add(new ProductOrder(breakingBad, BREAKING_BAD_ORDERED_QUANTITY + 1));
+
+        return Stream.of(
+                Arguments.of(firstShoppingBasket, new String[]{
+                        "Creation date of the shopping basket: 2023-07-16",
+                        "2 x The Hobbit // 2 x 5.00 = £10.00",
+                        "5 x Breaking Bad // 5 x 7.00 = £35.00",
+                        "Total: £45.00"
+                }),
+                Arguments.of(secondShoppingBasket, new String[]{
+                        "Creation date of the shopping basket: 2015-11-03",
+                        "1 x Alice in Wonderland // 1 x 12.80 = £12.80",
+                        "6 x Breaking Bad // 6 x 7.00 = £42.00",
+                        "Total: £54.80"
+                })
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("shoppingBasketProvider")
+    public void render_shopping_basket_to_the_console(ShoppingBasket shoppingBasket, String[] output) {
         renderer.render(shoppingBasket);
 
         // @todo Add InOrder
-        verify(console).printLine("Creation date of the shopping basket: 2023-07-16");
-        verify(console).printLine("2 x The Hobbit   // 2 x 5.00 = £10.00");
-        verify(console).printLine("5 x Breaking Bad // 5 x 7.00 = £35.00");
-        verify(console).printLine("Total: £45.00");
+        for (String line : output) {
+            verify(console).printLine(line);
+        }
     }
 }
